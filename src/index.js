ba392fd97e54b02f5af101f2e003535d87fa5c1b
joao Vitor <55700879+jao241@users.noreply.github.com>
2021-05-27 08:45:37 -0300

    Tipo Number

--- src/index.js
+++ src/index.js
@@ -12,3 +12,8 @@ console.log("Hello world");
 //String é um conjunto de caracteres: "abc"
 //É possivel utilizar aspas simples(''),
 //duplas(""), ou crases(``).
+
+//Number são valores numéricos: (1/ 1.5/ -4 ...)
+//Caso você tente uma operação que não retorne um nú_
+//mero ele retorna um NaN(Not a Number).
+console.log(NaN);
