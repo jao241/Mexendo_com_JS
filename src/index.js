b979268d0c380f22acfed787d63b07f62cac5601
joao Vitor <55700879+jao241@users.noreply.github.com>
2021-05-31 07:52:25 -0300

    Alterando a formatação dos comentários

--- src/index.js
+++ src/index.js
@@ -1,37 +1,46 @@
-//Comentários podem ser postos assim:
+/*
+Comentários podem ser postos assim:
 // ou /*  */
 
-//Exibindo mensagem no console.
-//console.log("Hello world");
-
-//É importante usar a sintexe de forma co_
-//rreta.
+/*
+Exibindo mensagem no console.
+console.log("Hello world");
+*/
+/*
+É importante usar a sintexe de forma correta.
+*/
 
 //Tipos de dados em JS.
 
-//Os dados podem ser divididos em dois grandes tipos: primitivos(
-// Number, String, Boolean, ...) e estruturais( object e functions)
-
-//String é um conjunto de caracteres: "abc"
-//É possivel utilizar aspas simples(''),
-//duplas(""), ou crases(``).
-
-//Number são valores numéricos: (1/ 1.5/ -4 ...)
-//Caso você tente uma operação que não retorne um nú_
-//mero ele retorna um NaN(Not a Number).
-//console.log(NaN);
-
-//Boolean é composto por dois valores: True or False
-//Muito utilizado para testes de condição.
-
-//Null e Undefined são dois tipos difentes de dados:
-//Null: indica uma variavel vazia, sem valor nela.
-//Undefined: indica uma resposta indefinida.
-
-//Objetos são representações de entidades do mundo real.
-//objetos podem ser compostos de atributos e métodos.
-//sua sintaxe é: {propriedade: "valor"}
-
+/*
+Os dados podem ser divididos em dois grandes tipos: primitivos(
+ Number, String, Boolean, ...) e estruturais( object e functions)
+*/
+/*
+String é um conjunto de caracteres: "abc"
+É possivel utilizar aspas simples(''),
+duplas(""), ou crases(``).
+*/
+/*
+Number são valores numéricos: (1/ 1.5/ -4 ...)
+Caso você tente uma operação que não retorne um nú_
+mero ele retorna um NaN(Not a Number).
+console.log(NaN);
+*/
+/*
+Boolean é composto por dois valores: True or False
+Muito utilizado para testes de condição.
+*/
+/*
+Null e Undefined são dois tipos difentes de dados:
+Null: indica uma variavel vazia, sem valor nela.
+Undefined: indica uma resposta indefinida.
+*/
+/*
+Objetos são representações de entidades do mundo real.
+objetos podem ser compostos de atributos e métodos.
+sua sintaxe é: {propriedade: "valor"}
+*/
 /*
 console.log({
   name: "Mayk",
@@ -42,8 +51,10 @@ console.log({
 });
 */
 
-//Array é um tipo de lista que armazena dados.
-//Sua sintaxe é: ['','', ...]
+/*
+Array é um tipo de lista que armazena dados.
+Sua sintaxe é: ['','', ...]
+*/
 /*
 console.log(["joao", "mateus", "douglas"])
 
